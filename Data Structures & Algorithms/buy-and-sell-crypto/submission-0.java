f1d6f7ddbc8a2d683820053bf3220c7517ddadf1
Keerthan karthikeya vaggala <keerthankarthikvaggala@gmail.com>
2026-07-23 14:22:14 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int start = 0;
+        int end = 1;
+        int max = 0;
+        while(end < prices.length){
+
+           if(prices[end]>prices[start]){
+            max = Math.max(max,prices[end] - prices[start]);
+            }
+            else{
+                start=end;
+            }
+            end++;
+        }
+        return max;
+    }
+}
